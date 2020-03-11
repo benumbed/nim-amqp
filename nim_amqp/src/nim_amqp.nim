1ed58112b2876cf7470644c067fd516fad0f17d1
Benumbed <benumbed@projectneutron.com>
2020-03-10 23:59:43 -0700

    I am dumb

--- nim_amqp/src/nim_amqp.nim
+++ nim_amqp/src/nim_amqp.nim
@@ -4,16 +4,29 @@
 ## (C) 2020 Benumbed (Nick Whalen) <benumbed@projectneutron.com> -- All Rights Reserved
 ##
 
+import strformat
 import net
-from asyncnet import nil
+import os
 
 when isMainModule:
-    let ssl_ctx = net.newContext()
-    var sock = asyncnet.newAsyncSocket()
-    asyncnet.wrapSocket(ssl_ctx, sock)
+    # let ssl_ctx = net.newContext()
+    # var sock = asyncnet.newAsyncSocket()
+    var sock = newSocket(buffered=true)
+    # asyncnet.wrapSocket(ssl_ctx, sock)
 
-    let con_futr = asyncnet.connect(sock, "172.29.42.39", Port(5672))
+    sock.connect("localhost", Port(5672), 5)
 
-    echo "SSL: ", asyncnet.isSsl(sock)
+    let header = "AMQP\0\0\9\1\c\l"
+    let sent = sock.trySend(header)
+
+    echo "buffered: ", sock.hasDataBuffered()
+
+    let data = sock.recv(8, 500)
+    let cr = int(data[3])
+    echo fmt"Data: {data[1]:#x} 7: {cr:#x}"
+
+    sock.close()
+
+    # asyncnet.close(sock)
     
     # asyncnet.close(sock)
