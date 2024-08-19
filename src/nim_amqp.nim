514871717873a112df2ea8d8cd9bc7413c7dd630
Benumbed <benumbed@projectneutron.com>
2024-08-19 01:14:43 -0700

    Stripping leading slash from the vhost in URI mode

--- src/nim_amqp.nim
+++ src/nim_amqp.nim
@@ -59,7 +59,14 @@ proc connect*(amqpUri: Uri, tuning = AMQPTuning()): AMQPConnection =
   ##
   ## tuning: AMQP tuning parameters, defaults to blank structure
   ##
-  connect(amqpUri.hostname, amqpUri.username, amqpUri.password, amqpUri.path, amqpUri.port.parseInt, useTls = if amqpUri.scheme == "amqps": true else: false)
+  connect(
+    amqpUri.hostname,
+    amqpUri.username,
+    amqpUri.password,
+    amqpUri.path.strip(trailing = false, chars = {'/'}),
+    amqpUri.port.parseInt,
+    useTls = if amqpUri.scheme == "amqps": true else: false
+  )
 
 
 proc reconnect*(conn: AMQPConnection) =
